33ca80647e117a23e23d80faa5f1c959df68e2e8
Kubra Yavuz <nurkacm@gmail.com>
2023-11-12 02:49:21 +0300

    first commit

---
+++ SepetManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+
+namespace Metotlar
+{
+	class SepetManager
+	{
+		public void Ekle(Urun urun)
+		{
+			Console.WriteLine("Tebrikler! Sepete eklendi:" + urun.Adi);
+		}
+
+		public void Ekle2(string urunAdi, string aciklama, double fiyat, int StokAdedi)
+		{
+			Console.WriteLine("Tebrikler! Sepete eklendi:" + urunAdi);
+        }
+		}
+	}
+	
+}
+
